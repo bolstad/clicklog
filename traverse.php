5f74a59a309900540842892bb40f0925bc395b12
Christian Bolstad <christian@carnaby.se>
2015-03-06 23:40:32 +0100

    lock to stable version of mvar/apache2-log-parser and skip kassner libs

--- traverse.php
+++ traverse.php
@@ -7,31 +7,54 @@ date_default_timezone_set( 'Europe/Stockholm' );
 use MVar\Apache2LogParser\AccessLogParser;
 use MVar\Apache2LogParser\LogIterator;
 
-$parser = new AccessLogParser( AccessLogParser::FORMAT_COMBINED );
+class clickLog {
+
+
+  private $parser;
+  private $lastEntry;
+  private $datafile;
+
+  function __construct( $file = '' ) {
+
+    $this->parser = new AccessLogParser( AccessLogParser::FORMAT_COMBINED );
+
+    if ( !file_exists( $file ) ) {
+      die( "uhm, $file does not exist\n" );
+    }
+
+    $this->datafile = $file;
+
+  }
+
+
+  function processLastEntry() {
+
+    print_r( $this->lastEntry );
+  }
+
+  function run() {
+    foreach ( new LogIterator( $this->datafile, $this->parser ) as $line => $data ) {
+
+      try {
+        $this->lastEntry = $this->parser->parseLine( $line );
+        $this->processLastEntry();
+
+      }
+      catch( MVar\Apache2LogParser\Exception\NoMatchesException $noMatch ) {
+
+      }
+    }
+
+
+  }
+}
+
 
 if ( empty( $argv[1] ) ) {
   die( "syntax: php $argv[0] filename\n" );
 }
 
 $file = $argv[1];
-if ( !file_exists( $file ) ) {
-  die( "uhm, $file does not exist\n" );
-}
 
-
-foreach ( new LogIterator( $file, $parser ) as $line => $data ) {
-  try {
-
-    $data = $parser->parseLine( $line );
-    print_r( $data );
-
-  }
-
-  catch( MVar\Apache2LogParser\Exception\NoMatchesException $noMatch ) {
-
-  }
-
-
-
-
-}
+$clickLog = new clickLog( $file );
+$clickLog->run();
